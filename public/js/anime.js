5cbe8dc0ce0c1280c8c378108626c3b68c208eb9
atio <atio@blizzard.com>
2015-05-25 13:39:13 -0700

    Initial display and filtering of anime

--- public/js/anime.js
+++ public/js/anime.js
@@ -1,11 +1,112 @@
 /** @jsx React.DOM */
+var tags = ['must-watch', 'action'];
 
+var AnimeSort = React.createClass({
+  updateAnime: function(tags) {
+    var activeTags = tags;
+    var newAnime = this.state.anime;
+    // if there are no active tags
+    if (activeTags.length == 0) {
+      // show all
+      _.map(newAnime, function(series){
+        series.isHidden = false;
+      });
+      return newAnime;
+    }
+
+    // iterate over each anime
+    _.map(newAnime, function(series) {
+      // let's assume the anime will not show
+      var isHidden = true;
+      // iterate over each anime's tags
+      series.tags.forEach(function(tag){
+        // if the tag is in our active tags, show the series
+        if(_.indexOf(activeTags, tag) >= 0) {
+          isHidden = false;
+        }
+      });
+      series.isHidden = isHidden;
+    });
+
+    return newAnime;
+  },
+  filterByTag: function(tag) {
+    var activeTags = this.state.activeTags;
+    var newAnime = [];
+    if (_.indexOf(activeTags, tag) >= 0) {
+      // remove it
+      _.remove(activeTags, tag);
+    } else {
+      // add it
+      activeTags.push(tag);
+    }
+    newAnime = this.updateAnime(activeTags);
+    this.setState({
+      activeTags: activeTags,
+      anime: newAnime
+    });
+    console.log("Filtering: " + tag);
+    console.log(this.state);
+  },
+  getInitialState: function() {
+    return {
+      activeTags:[],
+      anime:[]
+    };
+  },
+  componentWillMount: function() {
+    var initialAnime = this.props.anime;
+    _.map(initialAnime, function(anime){
+      anime.isHidden = false;
+    });
+    this.setState({
+      activeTags: [],
+      anime: initialAnime
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <TagFilters data={this.props.tags} tagHandler={this.filterByTag}/>
+        <AnimeList data={this.state.anime} />
+      </div>
+    );
+  }
+});
+
+// TagFilters Block
+var TagFilters = React.createClass({
+  render: function() {
+    var handler = this.props.tagHandler;
+    var tags = this.props.data.map(function(tag, i) {
+      return (
+        <TagButton key={i} data={tag} onUserCheck={handler}/>
+      )
+    });
+    return (
+      <ul>
+        {tags}
+      </ul>
+    );
+  }
+});
+
+var TagButton = React.createClass({
+  checkTag: function() {
+    if (this.refs[this.props.data].getDOMNode().checked) {
+      this.props.onUserCheck(this.refs[this.props.data].getDOMNode().value);
+    }
+  },
+  render: function() {
+    return <span>{this.props.data}<input type="checkbox" name={this.props.data} value={this.props.data} onChange={this.checkTag} ref={this.props.data} /></span>
+  }
+});
+
+// AnimeList Block
 var AnimeList = React.createClass({
   render: function() {
     var animeItems = this.props.data.map(function(anime){
-      return (
-        <Anime title={anime.name} description={anime.description} />
-      );
+      return (<Anime anime={anime} />);
     });
     return (
       <div>
@@ -17,15 +118,42 @@ var AnimeList = React.createClass({
 
 var Anime = React.createClass({
   render: function() {
+    var classString = "anime";
+    if (this.props.anime.isHidden) {
+      var classString = classString + " hidden";
+    }
     return (
-      <div>
-        <div>{this.props.title}</div>
-        <div>{this.props.description}</div>
+      <div className={classString}>
+        <div>{this.props.anime.name}</div>
+        <div>{this.props.anime.description}</div>
+        <AnimeTags tags={this.props.anime.tags} />
       </div>
     );
   }
 });
 
-React.render(<AnimeList data={window.anime.currentlyWatching} />, document.getElementById("now-watching"));
+var AnimeTags = React.createClass({
+  render: function() {
+    var handler = this.props.tagHandler;
+    var tags = this.props.tags.map(function(tag){
+      return (
+          <AnimeTag data={tag} />
+      );
+    });
+    return (
+      <div>
+        {tags}
+      </div>
+    );
+  }
+});
 
-React.render(<AnimeList data={window.anime.recommendations} />, document.getElementById("recommended"));
+var AnimeTag = React.createClass({
+  render: function() {
+    return (<span>{this.props.data}</span>);
+  }
+});
+
+// React.render(<AnimeList data={window.anime.currentlyWatching} />, document.getElementById("now-watching"));
+
+React.render(<AnimeSort anime={window.anime} tags={tags} />, document.getElementById("recommended"));
